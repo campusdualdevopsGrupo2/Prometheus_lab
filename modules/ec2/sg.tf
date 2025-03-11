2c876366b0910fed6660fa78bb04aa26b830e9e6
Jorge382 <jorge.vidal@campusdual.com>
2025-03-11 17:18:03 +0100

    Cambios en security group,docker compose y configuracón de prometheus

--- modules/ec2/sg.tf
+++ modules/ec2/sg.tf
@@ -20,6 +20,22 @@ resource "aws_security_group" "prometheus" {
   }*/
 
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Elasticsearch REST API"
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Elasticsearch REST API"
+  }
+
+  ingress {
     from_port   = 8000
     to_port     = 8000
     protocol    = "tcp"
